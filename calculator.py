614671a1358c854f41e0c4fb34b6e397987b596a
PyperDash <69603218+PyperDash@users.noreply.github.com>
2020-08-14 18:25:28 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+result = 0
+
+def calculate(num1, operator, num2):
+    if operator == '+':
+        result = num1 + num2
+        return result
+    elif operator == '-':
+        result = num1 - num2
+        return result
+    elif operator == '*':
+        result = num1 * num2
+        return result
+    elif operator == '/':
+        result = num1 / num2
+        return result
+    else:
+        result = 'Invalid operator'
+        return result
+
+print(calculate(2, '/', 3)) # Change the numbers and operator
